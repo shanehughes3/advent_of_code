86148d7a3b5d75d448093085e059ece4f3ae44db
Shane Hughes <shanehughes314@gmail.com>
2017-12-09 10:00:46 -0600

    day 8

---
+++ 2017/8/Day8.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.nio.file.*;
+import java.io.*;
+import java.util.regex.*;
+
+class Day8 {
+    public static void main(String[] args) {
+	CharStream strm = new CharStream();
+	System.out.printf("Garbage: %d\n", strm.clean());
+	System.out.printf("Value: %d\n", strm.parse());
+    }
+}
+
+class CharStream {
+    String input;
+    
+    public CharStream() {
+	try {
+	    input = new String(Files.readAllBytes(Paths.get("input.txt")));
+	} catch (IOException err) {
+	    System.err.println(err);
+	    System.exit(-1);
+	}
+    }
+
+    public int clean() {
+	removeCancelledChars();
+	return countAndRemoveGarbage();
+    }
+
+    private void removeCancelledChars() {
+	input = input.replaceAll("\\!.", "");
+    }
+
+    private int countAndRemoveGarbage() {
+	int total = 0;
+	Matcher garbage = Pattern.compile("<([^>]*)>").matcher(input);
+	while (garbage.find()) {
+	    total += garbage.group(1).length();
+	}
+	removeGarbage();
+	return total;
+    }
+
+    private void removeGarbage() {
+	input = input.replaceAll("<[^>]*>", "");
+    }
+
+    public int parse() {
+	int total = 0;
+	int depth = 0;
+	for (int i = 0; i < input.length(); ++i) {
+	    if (input.charAt(i) == '{') {
+		++depth;
+	    } else if (input.charAt(i) == '}') {
+		total += depth;
+		--depth;
+	    }
+	}
+	return total;
+    }
+}
